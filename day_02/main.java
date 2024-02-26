0591a47dfa0dd48eddcc64be7aa7346420f406c4
adarsh pathak <adarshpathak826@gmail.com>
2024-02-26 23:39:43 +0530

    Learn about java Data types!

---
+++ day_02/main.java
@@ -0,0 +1,12 @@
+public class main{
+    public static void main(String[] args){
+        // String name= "Tony Stark"
+        // int age = 43;
+        // double price =  25.23;
+        int a = 25;
+        int b = 10;
+
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
